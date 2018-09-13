3ac9ef8203be454d3f1ba5213d009efbfe46120f
foreveryennefer <whispersinthesoundofsilence2@gmail.com>
2018-09-13 01:56:08 +0100

    Adding goal scoring and lighting

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -5,8 +5,13 @@ extends Spatial
 # var b = "textvar"
 
 func _ready():
-	pass
+	$Panel.hide()
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().reload_current_scene()
+
+
+func _on_Area_body_entered(body):
+	if body is RigidBody:
+		$Panel.show()
